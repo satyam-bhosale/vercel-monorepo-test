a8a558d6a16ae7e9b2225b0283f6b76a9dcfd7c4
Satyam Bhosale <satyambhosale1404@gmail.com>
2026-06-20 04:01:17 +0530

    Register the auth endpoints and implement the cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,15 +1,33 @@
+import auth from "@axcelero/backend/lib/auth.js";
 import { toTitleCase } from "@axcelero/utils";
 import { hash } from "argon2";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(
+    "/api/auth/*", // or replace with "*" to enable cors for all routes
+    cors({
+        origin: "http://localhost:3001", // replace with your origin
+        allowHeaders: ["Content-Type", "Authorization"],
+        allowMethods: ["POST", "GET", "OPTIONS"],
+        exposeHeaders: ["Content-Length"],
+        maxAge: 600,
+        credentials: true,
+    }),
+);
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+    return auth.handler(c.req.raw);
+});
 
 app.get('/', (c) => {
     return c.text(toTitleCase("Hello Hono!"));
 });
 
 app.get('/users', async (c) => {
-    async function hashSample(){
+    async function hashSample() {
         const result = await hash("satyam");
         return result;
     }
