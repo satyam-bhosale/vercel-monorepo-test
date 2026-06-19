29410ad39d705a68a6276a9652afdd6cc87232c8
Satyam Bhosale <satyambhosale1404@gmail.com>
2026-06-20 01:38:22 +0530

    Add the argon 2

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,4 +1,5 @@
 import { toTitleCase } from "@axcelero/utils";
+import { hash } from "argon2";
 import { Hono } from "hono";
 
 const app = new Hono();
@@ -7,8 +8,14 @@ app.get('/', (c) => {
     return c.text(toTitleCase("Hello Hono!"));
 });
 
-app.get('/users', (c) => {
-    return c.text(toTitleCase("Hello Books!"));
+app.get('/users', async (c) => {
+    async function hashSample(){
+        const result = await hash("satyam");
+        return result;
+    }
+
+    const result = await hashSample();
+    return c.text(toTitleCase(`Hello Books! ${result}`));
 });
 
 export default app;
